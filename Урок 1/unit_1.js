a3b12bee87f946cb9bfdfc2a7f3ebcea52c7dd88
MikeGer92 <69321419+MikeGer92@users.noreply.github.com>
2021-03-22 15:37:34 +0300

    added lesson_1

---
+++ Урок 1/unit_1.js
@@ -0,0 +1,65 @@
+'use strict';
+
+console.log(leftBorderWidth);
+// console.log(second);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+console.log(second);
+
+// типы данных
+
+var number = 5;
+var string = 'Hello';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+// console.log(4 / 0);
+// console.log(string * 9);
+// let something
+// console.log(something);
+
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(person.name);
+// console.log(person['age']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[2]);
+
+// alert('Hello World');
+
+// confirm('Are you here?');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = prompt('Есть ли Вам 18 лет?', 'Да');
+// console.log(typeof (answer));
+// console.log('arr' + '- object');
+// console.log(180 + +'- object');
+// console.log(typeof (180 + +'- object'));
+
+let incr = 10,
+    decr = 10;
+
+// console.log(++incr); на выходе 11
+// console.log(--decr); на выходе 9
+// console.log(incr++); // на выходе 10
+// console.log(decr--); // на выходе 10
+
+// console.log(5 % 2);
+// console.log(5 == 2);
+// console.log('2' == 2);
+// console.log('2' === 2);
