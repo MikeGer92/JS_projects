726a5c51475d7145a908fe4aaa3627d42da35a89
MikeGer <mikegera@yandex.ru>
2021-03-23 13:26:54 +0300

    finished task_2

--- Уроки/Урок_1/task_2.js
+++ Уроки/Урок_1/task_2.js
@@ -11,8 +11,33 @@ let appData = {
     timeData: time,
     savings: false
 };
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
 
-for (let i = 0; i < 2; i++) {
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+//         (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log('OK!');
+//         appData.expenses[a] = b;
+//     } else {
+
+//     }
+// }
+// let i = 0
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+//         (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log('OK!');
+//         appData.expenses[a] = b;
+//         i++;
+//     } else {
+//     }
+// }
+let i = 0;
+do {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется?", '');
 
@@ -20,10 +45,11 @@ for (let i = 0; i < 2; i++) {
         (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
         console.log('OK!');
         appData.expenses[a] = b;
-    } else {
-
-    }
+        i++;
+    } else {}
 }
+while (i < 2);
+
 appData.moneyPerDay = appData.budget / 30;
 alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
