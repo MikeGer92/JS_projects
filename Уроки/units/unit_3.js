b6e1cfa3ba51dadb4745c44a776cb026d1958b6f
MikeGer <mikegera@yandex.ru>
2021-03-24 17:44:14 +0300

    finished unit_4-5

--- Уроки/units/unit_3.js
+++ Уроки/units/unit_3.js
@@ -1,56 +1,65 @@
-let money, time;
+// let money, time;
 
-function start() {
-    money = +prompt('Введите Ваш бюджет на месяц:', '');
-    while (isNaN(money) || money == '' || money == null) {
-        money = +prompt('Введите Ваш бюджет на месяц:', '');
-    }
-    time = prompt('Введите дату в формате YYYY-MM-DD:', '');
-}
-start();
-let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: time,
-    savings: true
-};
+// function start() {
+//     money = +prompt('Введите Ваш бюджет на месяц:', '');
+//     while (isNaN(money) || money == '' || money == null) {
+//         money = +prompt('Введите Ваш бюджет на месяц:', '');
+//     }
+//     time = prompt('Введите дату в формате YYYY-MM-DD:', '');
+// }
+// start();
+// let appData = {
+//     budget: money,
+//     expenses: {},
+//     optionalExpenses: {},
+//     income: [],
+//     timeData: time,
+//     savings: true
+// };
 
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
-        if ((typeof (a)) === 'string' && (typeof (a)) != null &&
-            (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-            console.log('OK!');
-            appData.expenses[a] = b;
-        } else {
-            console.log("bad result");
-            i--;
-        }
-    }
-}
-chooseExpenses();
+// function chooseExpenses() {
+//     for (let i = 0; i < 2; i++) {
+//         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//             b = prompt("Во сколько обойдется?", '');
+//         if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+//             (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//             console.log('OK!');
+//             appData.expenses[a] = b;
+//         } else {
+//             console.log("bad result");
+//             i--;
+//         }
+//     }
+// }
+// chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed(2);
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+// appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+// alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка');
-}
+// if (appData.moneyPerDay < 100) {
+//     console.log('Минимальный уровень достатка');
+// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+//     console.log('Средний уровень достатка');
+// } else if (appData.moneyPerDay > 2000) {
+//     console.log('Высокий уровень достатка');
+// }
 
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt('Какова сумма накоплений: ', ''),
-            percent = +prompt('Под какой процент?', '');
+// function checkSavings() {
+//     if (appData.savings == true) {
+//         let save = +prompt('Какова сумма накоплений: ', ''),
+//             percent = +prompt('Под какой процент?', '');
 
-        appData.monthIncome = save / 100 / 12 * percent;
-        alert('Доход с Вашего депозита в месяц: ' + appData.monthIncome);
-    }
-}
-checkSavings();
+//         appData.monthIncome = save / 100 / 12 * percent;
+//         alert('Доход с Вашего депозита в месяц: ' + appData.monthIncome);
+//     }
+// }
+// checkSavings();
+// let arr = [5, 11, 1, 15, 3, 26, 8, 4],
+//     i = arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// console.log(arr);
+console.log(0 || "" || 2 || undefined || true || false);
---
+++ Уроки/units/unit_4.js
@@ -0,0 +1,39 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'pink';
+btn[1].style.borderRadius = '100%';
+btn[3].style.height = '80px';
+btn[4].style.backgroundColor = 'lightblue';
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'brown';
+// };
+// heart.forEach(function (item, i, hearts) {
+//     item.style.backgroundColor = 'blue';
+// });
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я!');
+
+div.classList.add('black');
+
+document.body.appendChild(div);
+// wrapper.appendChild(div);
+
+// div.innerHTML = '<h1> Hello World</h1>';
+
+div.textContent = 'Hello World';
+
+// document.body.insertBefore(div, circle[1]);
+// document.body.removeChild(btn[2]);
+// document.body.replaceChild(circle[1], btn[1]);
+
+
+console.log(text);
